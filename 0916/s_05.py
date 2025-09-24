1e9e182b36815f81e0c9d268af74478dcb098e13
dotori200 <winboy731@gmail.com>
2025-09-24 10:52:26 +0900

    ddd

--- 0916/s_05.py
+++ 0916/s_05.py
@@ -12,3 +12,12 @@ with st.container(horizontal=True,
     st.button("Button 8")
     st.button("Button 9")
     st.button("Button 10")
+
+  
+st.title("버튼 예제")
+
+# 버튼 생성
+if st.button("눌러보세요!"):
+    st.write("버튼이 눌렸습니다 🎉")
+else:
+    st.write("아직 안 눌림 😅")
--- 0923/streamlit_01_ver1.py
+++ 0923/streamlit_01_ver1.py
@@ -4,8 +4,15 @@ import pandas as pd
 from sqlalchemy import create_engine, text
 import os
 
-st.set_page_config(page_title="Kia FAQ TOP10", layout="wide")
-st.title("🚗 Kia FAQ TOP 10")
+st.set_page_config(page_title="차량 FAQ", layout="wide")
+st.title("🚗 차량 FAQ")
+
+with st.container(horizontal=True, 
+                  gap="medium",border=True
+                  ):
+    st.button("ALL")
+    st.button("전기차")
+    st.button("하이브리드")
 
 DB_URL = os.getenv("DB_URL", "mysql+pymysql://root:root1234@localhost:3306/car_db?charset=utf8mb4")
 engine = create_engine(DB_URL)
@@ -24,3 +31,4 @@ df = pd.DataFrame(rows, columns=["company","category","question","answer"])
 for i, r in df.iterrows():
     with st.expander(f"{i+1}. {r['question']}"):
         st.write(r['answer'])
+
